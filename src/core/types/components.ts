9b0c909c896000f92eff41fc711d5372ddd1a2c2
Geirr Winnem <g.winnem@ringana.com>
2023-02-10 07:42:17 +0100

    Removed unused Type

--- src/core/types/components.ts
+++ src/core/types/components.ts
@@ -1,5 +1,3 @@
 type TInnerKeys = `h` | `w` | `x` | `y`;
 export type TGridLayoutEvent = [string, number, number, number, number, number];
 export type TInner<Type> = Record<TInnerKeys, Type>;
-// eslint-disable-next-line no-undef
-export type TIntersectionObserverConfig = IntersectionObserverInit;
