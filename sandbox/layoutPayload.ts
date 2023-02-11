0d8b4b2641aff2af1fbb79a0de1bb99ea76e0517
Geirr Winnem <g.winnem@ringana.com>
2023-02-11 08:07:26 +0100

    New demo payload layout

--- sandbox/layoutPayload.ts
+++ sandbox/layoutPayload.ts
@@ -1,76 +1,24 @@
 import { TLayout } from '../src/core/types/helpers';
 
 export const data: TLayout = [
-  {
-    x: 0,
-    y: 0,
-    w: 1,
-    h: 1,
-    i: 0,
-    static: true
-  },
-  {
-    x: 2,
-    y: 0,
-    w: 1,
-    h: 1,
-    i: 1
-  },
-  {
-    x: 4,
-    y: 0,
-    w: 1,
-    h: 2,
-    i: 2
-  },
-  {
-    x: 0,
-    y: 2,
-    w: 1,
-    h: 2,
-    i: 3
-  },
-  {
-    x: 2,
-    y: 2,
-    w: 2,
-    h: 2,
-    i: 4,
-    static: true
-  },
-  {
-    x: 4,
-    y: 2,
-    w: 2,
-    h: 2,
-    i: 5
-  },
-  {
-    x: 0,
-    y: 4,
-    w: 2,
-    h: 2,
-    i: 6
-  },
-  {
-    x: 2,
-    y: 4,
-    w: 2,
-    h: 2,
-    i: 7
-  },
-  {
-    x: 4,
-    y: 4,
-    w: 2,
-    h: 2,
-    i: 8
-  },
-  {
-    x: 0,
-    y: 6,
-    w: 2,
-    h: 2,
-    i: 9
-  }
+  {"x":0,"y":0,"w":2,"h":2,"i":0, static: false},
+  {"x":2,"y":0,"w":2,"h":4,"i":1, static: true},
+  {"x":4,"y":0,"w":2,"h":5,"i":2, static: false},
+  {"x":6,"y":0,"w":2,"h":3,"i":3, static: false},
+  {"x":8,"y":0,"w":2,"h":3,"i":4, static: false},
+  {"x":10,"y":0,"w":2,"h":3,"i":5, static: false},
+  {"x":0,"y":5,"w":2,"h":5,"i":6, static: false},
+  {"x":2,"y":5,"w":2,"h":5,"i":7, static: false},
+  {"x":4,"y":5,"w":2,"h":5,"i":8, static: false},
+  {"x":6,"y":3,"w":2,"h":4,"i":9, static: true},
+  {"x":8,"y":4,"w":2,"h":4,"i":10, static: false},
+  {"x":10,"y":4,"w":2,"h":4,"i":11, static: false},
+  {"x":0,"y":10,"w":2,"h":5,"i":12, static: false},
+  {"x":2,"y":10,"w":2,"h":5,"i":13, static: false},
+  {"x":4,"y":8,"w":2,"h":4,"i":14, static: false},
+  {"x":6,"y":8,"w":2,"h":4,"i":15, static: false},
+  {"x":8,"y":10,"w":2,"h":5,"i":16, static: false},
+  {"x":10,"y":4,"w":2,"h":2,"i":17, static: false},
+  {"x":0,"y":9,"w":2,"h":3,"i":18, static: false},
+  {"x":2,"y":6,"w":2,"h":2,"i":19, static: false}
 ];
