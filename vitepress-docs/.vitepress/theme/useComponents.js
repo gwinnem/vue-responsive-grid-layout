7c055ee8838a074a4a465a9706b4f2efbb38fd5c
Geirr Winnem <g.winnem@ringana.com>
2023-02-15 06:10:29 +0100

    vitepress config

--- vitepress-docs/.vitepress/theme/useComponents.js
+++ vitepress-docs/.vitepress/theme/useComponents.js
@@ -1,7 +1,5 @@
 // Don't remove this file, because it registers the demo components.
-import { GridLayout, GridItem } from '../../../dist/vue-fluid-grid-layout.mjs';
-
-// import Example02Events fro
+import { GridLayout, GridItem } from '../../../dist/vue-responsive-grid-layout.mjs';
 
 export function useComponents(app) {
   app.component('grid-layout', GridLayout);
