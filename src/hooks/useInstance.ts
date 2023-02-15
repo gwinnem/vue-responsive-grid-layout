0d3b0d318783be5c506d840d544676fe79755fec
Geirr Winnem <g.winnem@ringana.com>
2023-02-15 06:08:57 +0100

    lint fixing

--- src/hooks/useInstance.ts
+++ src/hooks/useInstance.ts
@@ -1,13 +1,13 @@
-import {ComponentInternalInstance, getCurrentInstance} from "vue"
+import { ComponentInternalInstance, getCurrentInstance } from 'vue';
 
-export default function useCurrentInstance() {
-  const {appContext, proxy} = getCurrentInstance() as ComponentInternalInstance
+export default function useCurrentInstance(): any {
+  const { appContext, proxy } = getCurrentInstance() as ComponentInternalInstance;
 
-  const globalProperties = appContext.config.globalProperties
+  const { globalProperties } = appContext.config;
 
   return {
-    proxy,
     appContext,
-    globalProperties
-  }
+    globalProperties,
+    proxy,
+  };
 }
