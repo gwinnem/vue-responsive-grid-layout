4d5e00cff9870201f846d732a83c1a881ced6a3c
Geirr Winnem <g.winnem@ringana.com>
2023-02-15 06:28:54 +0100

    Updated exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,17 +1,27 @@
-import { App } from 'vue';
 import GridItem from './Grid/GridItem.vue';
 import GridLayout from './Grid/GridLayout.vue';
 
-export { GridItem, GridLayout };
-
-const components = [GridLayout, GridItem];
-
-const VueResponsiveGridLayout = {
-  install(App: App): void {
-    components.forEach(item => {
-      App.component(item.name, item);
-    });
-  },
+const components = {
+  GridItem,
+  GridLayout,
 };
 
-export default VueResponsiveGridLayout;
+function install(app: any): void {
+  if(app.VueResponsiveGridLayout) return;
+
+  app.VueResponsiveGridLayout = true;
+  (Object.keys(components) as (keyof typeof components)[]).forEach(name => {
+    app.component(name, components[name]);
+  });
+}
+
+export {
+  GridItem,
+  GridLayout,
+};
+
+export default {
+  GridItem,
+  GridLayout,
+  install,
+};
