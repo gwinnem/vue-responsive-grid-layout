2876ea82a74169b1d2166abed6fc0a9189f568b4
Geirr Winnem <g.winnem@ringana.com>
2023-02-21 10:13:35 +0100

    Moved example layout and css to separate files

---
+++ vitepress-docs/.vitepress/theme/components/example-layout.ts
@@ -0,0 +1,162 @@
+export const layout = [
+  {
+    "x": 0,
+    "y": 0,
+    "w": 2,
+    "h": 2,
+    "i": "0",
+    isStatic: false
+  },
+  {
+    "x": 2,
+    "y": 0,
+    "w": 2,
+    "h": 4,
+    "i": "1",
+    isStatic: true
+  },
+  {
+    "x": 4,
+    "y": 0,
+    "w": 2,
+    "h": 5,
+    "i": "2",
+    isStatic: false
+  },
+  {
+    "x": 6,
+    "y": 0,
+    "w": 2,
+    "h": 3,
+    "i": "3",
+    isStatic: false
+  },
+  {
+    "x": 8,
+    "y": 0,
+    "w": 2,
+    "h": 3,
+    "i": "4",
+    isStatic: false
+  },
+  {
+    "x": 10,
+    "y": 0,
+    "w": 2,
+    "h": 3,
+    "i": "5",
+    isStatic: false
+  },
+  {
+    "x": 0,
+    "y": 5,
+    "w": 2,
+    "h": 5,
+    "i": "6",
+    isStatic: false
+  },
+  {
+    "x": 2,
+    "y": 5,
+    "w": 2,
+    "h": 5,
+    "i": "7",
+    isStatic: false
+  },
+  {
+    "x": 4,
+    "y": 5,
+    "w": 2,
+    "h": 5,
+    "i": "8",
+    isStatic: false
+  },
+  {
+    "x": 6,
+    "y": 3,
+    "w": 2,
+    "h": 4,
+    "i": "9",
+    isStatic: true
+  },
+  {
+    "x": 8,
+    "y": 4,
+    "w": 2,
+    "h": 4,
+    "i": "10",
+    isStatic: false
+  },
+  {
+    "x": 10,
+    "y": 4,
+    "w": 2,
+    "h": 4,
+    "i": "11",
+    isStatic: false
+  },
+  {
+    "x": 0,
+    "y": 10,
+    "w": 2,
+    "h": 5,
+    "i": "12",
+    isStatic: false
+  },
+  {
+    "x": 2,
+    "y": 10,
+    "w": 2,
+    "h": 5,
+    "i": "13",
+    isStatic: false
+  },
+  {
+    "x": 4,
+    "y": 8,
+    "w": 2,
+    "h": 4,
+    "i": "14",
+    isStatic: false
+  },
+  {
+    "x": 6,
+    "y": 8,
+    "w": 2,
+    "h": 4,
+    "i": "15",
+    isStatic: false
+  },
+  {
+    "x": 8,
+    "y": 10,
+    "w": 2,
+    "h": 5,
+    "i": "16",
+    isStatic: false
+  },
+  {
+    "x": 10,
+    "y": 4,
+    "w": 2,
+    "h": 2,
+    "i": "17",
+    isStatic: false
+  },
+  {
+    "x": 0,
+    "y": 9,
+    "w": 2,
+    "h": 3,
+    "i": "18",
+    isStatic: false
+  },
+  {
+    "x": 2,
+    "y": 6,
+    "w": 2,
+    "h": 2,
+    "i": "19",
+    isStatic: false
+  }
+];
