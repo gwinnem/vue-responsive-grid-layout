b1c271a09848c3c7a3eb169a99af266503dbdf58
Geirr Winnem <g.winnem@ringana.com>
2023-02-25 07:34:33 +0100

    Updated test layout.

--- sandbox/test.ts
+++ sandbox/test.ts
@@ -1,9 +1,9 @@
 import { TLayout } from "../src/components/Grid/layout-definition";
 
 export const testData: TLayout = [
-  { x:0, y:0, w:2, h:2, i:0, isDraggable: true },
+  { x:0, y:0, w:2, h:2, i:0, isDraggable: false },
   { x:2, y:0, w:2, h:2, i:1, isStatic: true },
-  { x:4, y:0, w:2, h:2, i:2 },
+  { x:4, y:0, w:2, h:2, i:2, isResizable: false },
   { x:0, y:2, w:2, h:2, i:3 },
   { x:2, y:2, w:2, h:2, i:4 },
   { x:4, y:2, w:2, h:2, i:5 },
