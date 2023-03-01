2b8eaac65408e38a9280b6ed44406a22a91cfe58
Geirr Winnem <g.winnem@ringana.com>
2023-03-01 06:46:12 +0100

    Commented imports from package

--- sandbox/App.vue
+++ sandbox/App.vue
@@ -146,11 +146,11 @@
 <script lang="ts" setup>
   import { ref, onMounted, nextTick, onBeforeUnmount, computed } from 'vue';
   import { testData } from './test';
-  // import GridLayout from '../src/components/Grid/GridLayout.vue';
-  // import GridItem  from '../src/components/Grid/GridItem.vue';
-  // import { TLayoutItem } from '../src/components/Grid/layout-definition';
-  import { GridLayout, GridItem } from 'vue-ts-responsive-grid-layout';
-  import { TLayoutItem } from 'vue-ts-responsive-grid-layout';
+  import GridLayout from '../src/components/Grid/GridLayout.vue';
+  import GridItem  from '../src/components/Grid/GridItem.vue';
+  import { TLayoutItem } from '../src/components/Grid/layout-definition';
+  // import { GridLayout, GridItem } from 'vue-ts-responsive-grid-layout';
+  // import { TLayoutItem } from 'vue-ts-responsive-grid-layout';
   // import type { TLayout } from 'vue-ts-responsive-grid-layout';
 
   const autoResizeGridLayout = ref(true);
