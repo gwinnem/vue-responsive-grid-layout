63b2b0874c390757993b09d0260851744274f1d7
Geirr Winnem <g.winnem@ringana.com>
2023-03-01 06:38:41 +0100

    Fixed css issue

--- sandbox/App.vue
+++ sandbox/App.vue
@@ -146,14 +146,14 @@
 <script lang="ts" setup>
   import { ref, onMounted, nextTick, onBeforeUnmount, computed } from 'vue';
   import { testData } from './test';
-  import GridLayout from '../src/components/Grid/GridLayout.vue';
-  import GridItem  from '../src/components/Grid/GridItem.vue';
-  import { TLayoutItem } from '../src/components/Grid/layout-definition';
-  // import { GridLayout, GridItem } from 'vue-ts-responsive-grid-layout';
-  // import { TLayoutItem } from 'vue-ts-responsive-grid-layout';
+  // import GridLayout from '../src/components/Grid/GridLayout.vue';
+  // import GridItem  from '../src/components/Grid/GridItem.vue';
+  // import { TLayoutItem } from '../src/components/Grid/layout-definition';
+  import { GridLayout, GridItem } from 'vue-ts-responsive-grid-layout';
+  import { TLayoutItem } from 'vue-ts-responsive-grid-layout';
   // import type { TLayout } from 'vue-ts-responsive-grid-layout';
 
-  const autoResizeGridLayout = ref(false);
+  const autoResizeGridLayout = ref(true);
   const borderRadiusPx = ref(8);
   const colNum = ref(12);
   const enableEditMode = ref(true);
