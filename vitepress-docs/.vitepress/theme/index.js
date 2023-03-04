d5888f9f70c63b95e60fa46c4d86265f3aead3b9
Geirr Winnem <g.winnem@ringana.com>
2023-03-04 10:02:24 +0100

    NA

--- vitepress-docs/.vitepress/theme/index.js
+++ vitepress-docs/.vitepress/theme/index.js
@@ -1,14 +1,14 @@
 import DefaultTheme from 'vitepress/theme';
 // import { useComponents } from './useComponents';
 // import './styles/index.css';
-// import { GridItem, GridLayout } from '@/components/index';
+//import { * } from '../../examples/01-example.vue';
 
 export default {
   ...DefaultTheme,
   enhanceApp(ctx) {
     DefaultTheme.enhanceApp(ctx);
-   // ctx.app.component('GridLayout', GridLayout);
-   // ctx.app.component('GridItem', GridItem);
+//   ctx.app.component('GridLayout', GridLayout);
+//   ctx.app.component('GridItem', GridItem);
     // useComponents(ctx.app);
   },
 };
