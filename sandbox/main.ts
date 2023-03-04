cf225cf4147421da4ac2a7e45c1190e437286d36
Geirr Winnem <g.winnem@ringana.com>
2023-03-04 10:00:05 +0100

    Updated imports

--- sandbox/main.ts
+++ sandbox/main.ts
@@ -1,8 +1,6 @@
 import App from './App.vue'
 // @ts-ignore
-// import { GridLayout, GridItem, CustomCloseButton, CustomDragElement } from 'vue-ts-responsive-grid-layout';
-import GridLayout from '../src/components/Grid/GridLayout.vue';
-import GridItem  from '../src/components/Grid/GridItem.vue';
+import { GridLayout, GridItem, CustomCloseButton, CustomDragElement } from 'vue-ts-responsive-grid-layout';
 import { createApp } from 'vue'
 
 const app = createApp(App)
