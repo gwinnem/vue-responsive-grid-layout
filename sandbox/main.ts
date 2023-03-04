b7026f4bbf5da9ed88df612c478fbc1518845f7e
Geirr Winnem <g.winnem@ringana.com>
2023-03-04 12:46:18 +0100

    Removed obsolete imports

--- sandbox/main.ts
+++ sandbox/main.ts
@@ -1,18 +1,7 @@
 import App from './App.vue'
-// @ts-ignore
-import { GridLayout, GridItem, CustomCloseButton, CustomDragElement } from 'vue-ts-responsive-grid-layout';
 import { createApp } from 'vue'
 
 const app = createApp(App)
 
-app.use(GridLayout)
-app.use(GridItem)
-
 app.mount('#app')
 
-export default {
-  // CustomCloseButton,
-  // CustomDragElement,
-  GridItem,
-  GridLayout
-};
