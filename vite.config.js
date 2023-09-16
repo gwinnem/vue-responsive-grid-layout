361ef8c40f464dbed294a8f03b28db414b427a12
Geirr Winnem <g.winnem@ringana.com>
2023-09-16 05:26:58 +0200

    Added new plugins

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import * as path from 'path';
 import vue from '@vitejs/plugin-vue';
+import libCss from 'vite-plugin-libcss';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
@@ -27,6 +29,8 @@ export default defineConfig({
   define: { 'process.env': {} },
   plugins: [
     vue(),
+    libCss(),
+    dts(),
   ],
   resolve: {
     alias: {
