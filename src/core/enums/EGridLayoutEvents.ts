a298d523004a3280a5dcdb63f37b60ea9bc768b7
gwinnem <gwinnem@gmail.com>
2023-10-18 08:14:47 +0200

    Renamed UPDATE_LAYOUT and added DRAG_MOVE

--- src/core/enums/EGridLayoutEvents.ts
+++ src/core/enums/EGridLayoutEvents.ts
@@ -7,11 +7,12 @@ export enum EGridLayoutEvent {
   COLUMNS_CHANGED = `columns-changed`,
   CONTAINER_RESIZED = `container-resized`,
   DRAG_END = `drag-end`,
+  DRAG_MOVE= `drag-move`,
   DRAG_START = `drag-start`,
   LAYOUT_BEFORE_MOUNT = `layout-before-mount`,
   LAYOUT_CREATED = `layout-created`,
   LAYOUT_MOUNTED = `layout-mounted`,
   LAYOUT_READY = `layout-ready`,
   LAYOUT_UPDATED = `layout-updated`,
-  UPDATE_LAYOUT = `layout-update`,
+  LAYOUT_UPDATE = `layout-update`,
 }
