c19acf2fa31891702bcb94159267a30cde0c64ea
gwinnem <gwinnem@gmail.com>
2023-10-19 15:18:35 +0200

    Removed obsolete file.

--- vitepress-docs/.vitepress/theme/useComponents.js
+++
@@ -1,6 +0,0 @@
-import { GridLayout, GridItem } from '/vue-ts-responsive-grid-layout.';
-
-export function useComponents(app) {
-  app.component('grid-layout', GridLayout);
-  app.component('grid-item', GridItem);
-}
