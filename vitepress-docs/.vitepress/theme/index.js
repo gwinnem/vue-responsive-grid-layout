2f5a6dd1b0bdc761495faa5a658331743b330541
gwinnem <gwinnem@gmail.com>
2023-10-19 15:17:50 +0200

    Removed commented code.

--- vitepress-docs/.vitepress/theme/index.js
+++ vitepress-docs/.vitepress/theme/index.js
@@ -1,13 +1,9 @@
 import DefaultTheme from 'vitepress/theme';
-// import { useComponents } from './useComponents';
 import './styles/index.css';
 
 export default {
   ...DefaultTheme,
   enhanceApp(ctx) {
     DefaultTheme.enhanceApp(ctx);
-//   ctx.app.component('GridLayout', GridLayout);
-//   ctx.app.component('GridItem', GridItem);
-    // useComponents(ctx.app);
   },
 };
