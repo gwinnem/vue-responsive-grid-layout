2d775481022b70f9aba9631b3c9e51d5be577ba8
gwinnem <gwinnem@gmail.com>
2023-10-19 19:29:46 +0200

    Added documentation

--- src/core/helpers/DOM.ts
+++ src/core/helpers/DOM.ts
@@ -1,7 +1,16 @@
+/**
+ * Checking if the window object exists.
+ */
 const hasWindow = (): boolean => {
   return typeof window !== `undefined`;
 };
 
+/**
+ * Adding a event listener to the window object.
+ * @param {String}    event     Name of the event to listen for.
+ * @param {Function}  callback  Callback function.
+ * @see   https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+ */
 export const addWindowEventListener = (event: string, callback: () => any): boolean => {
   if(!hasWindow) {
     callback();
@@ -11,6 +20,12 @@ export const addWindowEventListener = (event: string, callback: () => any): bool
   return true;
 };
 
+/**
+ * Removing a event listener from the window object.
+ * @param {String}    event     Name of the event to remove.
+ * @param {Function}  callback  Callback function.
+ * @see   https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/removeEventListener
+ */
 export const removeWindowEventListener = (event: string, callback: () => any): void => {
   if(!hasWindow) {
     return;
