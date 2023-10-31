20d0ad3a46dfcb34007bb74a3deaf6b3ace4e4fd
gwinnem <gwinnem@gmail.com>
2023-10-31 11:46:42 +0100

    Test: New test skeleton

---
+++ tests/responsiveUtils.spec.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import { describe, expect, it } from 'vitest';
+// import { sortBreakpoints } from '@/core/helpers/responsiveUtils';
+
+describe(`Testing responsiveUtils`, () => {
+  it(`Breakpoints are ok`, () => {
+    expect(true).toBe(false);
+  });
+});
