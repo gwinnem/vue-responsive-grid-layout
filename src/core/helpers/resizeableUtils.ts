8a9d9c7a1b6d8ebe6ae7f52496481678f2b891ee
gwinnem <gwinnem@gmail.com>
2023-12-02 14:23:40 +0100

    :recycle: (resizeAbleUtils) Empty file
    File added for future refactorings

---
+++ src/core/helpers/resizeableUtils.ts
@@ -0,0 +1,23 @@
+// export const setEnabledResizeOptions = () => {
+//     const opts = {
+//         edges: {
+//             bottom: true,
+//             left: false,
+//             right: true,
+//             top: false,
+//         },
+//         ignoreFrom: props.resizeIgnoreFrom,
+//         modifiers: [],
+//         restrictSize: {
+//             max: {
+//                 height: maximum.height * transformScale.value,
+//                 width: maximum.width * transformScale.value,
+//             },
+//             min: {
+//                 height: minimum.height * transformScale.value,
+//                 width: minimum.width * transformScale.value,
+//             },
+//         },
+//         ...props.resizeOption,
+//     };
+// }
