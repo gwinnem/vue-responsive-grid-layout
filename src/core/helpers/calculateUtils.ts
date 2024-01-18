a1b02542bdb1446609580088a6b6a6b365f3493b
gwinnem <gwinnem@gmail.com>
2024-01-18 17:20:59 +0100

    Refactored function and added more tests to it

--- src/core/helpers/calculateUtils.ts
+++ src/core/helpers/calculateUtils.ts
@@ -1,6 +1,49 @@
 import {ICalcXy} from '@/core/griditem/interfaces/grid-item.interfaces';
 import {calcColWidth} from "@/core/griditem/helpers/gridItemCalculateHelper";
+import {ErrorMsg} from "@/core/common/enums/ErrorMessages";
 
+
+const validateXYParams = (
+  rowHeight: number,
+  margin: [x: number, y: number],
+  cols: number,
+  innerH: number,
+  innerW: number,
+  maxRows: number,
+  containerWidth: number): void => {
+
+  if (rowHeight < 1) {
+    throw new Error(ErrorMsg.INVALID_PARAM_ROW_HEIGHT);
+  }
+
+  if (margin[0] < 1 || margin[1] < 1) {
+    throw new Error(ErrorMsg.INVALID_PARAM_MARGIN);
+  }
+
+  if (cols < 1) {
+    throw new Error(ErrorMsg.INVALID_PARAM_COLS);
+  }
+
+  if (innerH < 1) {
+    throw new Error(ErrorMsg.INVALID_PARAM_INNER_H);
+  }
+
+  if (innerW < 1) {
+    throw new Error(ErrorMsg.INVALID_PARAM_INNER_W);
+  }
+
+  if (cols < 1) {
+    throw new Error(ErrorMsg.INVALID_PARAM_COLS);
+  }
+
+  if (maxRows < 1) {
+    throw new Error(ErrorMsg.INVALID_PARAM_MAX_ROWS);
+  }
+
+  if (containerWidth < 1) {
+    throw new Error(ErrorMsg.INVALID_PARAM_CONTAINER_WIDTH);
+  }
+};
 /**
  * Translate x and y coordinates from pixels to grid units.
  * @param   {Number}    top             Top position (relative to parent) in pixels.
@@ -15,26 +58,29 @@ import {calcColWidth} from "@/core/griditem/helpers/gridItemCalculateHelper";
  * @return  {ICalcXy}                   x and y in grid units.
  */
 export const calcXY = (
-    top: number,
-    left: number,
-    margin: [x: number, y: number],
-    rowHeight: number,
-    cols: number,
-    innerH: number,
-    innerW: number,
-    maxRows: number,
-    containerWidth: number
+  top: number,
+  left: number,
+  margin: [x: number, y: number],
+  rowHeight: number,
+  cols: number,
+  innerH: number,
+  innerW: number,
+  maxRows: number,
+  containerWidth: number
 ): ICalcXy => {
-    const colWidth = calcColWidth(containerWidth, margin[0], cols);
-    let x = Math.round((left - margin[0]) / (colWidth + margin[0]));
-    let y = Math.round((top - margin[1]) / (rowHeight + margin[1]));
 
-    // Capping
-    x = Math.max(Math.min(x, cols - innerW), 0);
-    y = Math.max(Math.min(y, maxRows - innerH), 0);
+  validateXYParams(rowHeight, margin,cols,innerH, innerW, maxRows, containerWidth);
 
-    return {
-        x,
-        y,
-    };
+  const colWidth = calcColWidth(containerWidth, margin[0], cols);
+  let x = Math.round((left - margin[0]) / (colWidth + margin[0]));
+  let y = Math.round((top - margin[1]) / (rowHeight + margin[1]));
+
+  // Capping
+  x = Math.max(Math.min(x, cols - innerW), 0);
+  y = Math.max(Math.min(y, maxRows - innerH), 0);
+
+  return {
+    x,
+    y,
+  };
 };
--- tests/calculateUtils.spec.ts
+++ tests/calculateUtils.spec.ts
@@ -1,10 +1,52 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import { describe, expect, it } from 'vitest';
+import {describe, expect, it} from 'vitest';
 import {calcXY} from "../src/core/helpers/calculateUtils";
+import {ErrorMsg} from "../src/core/common/enums/ErrorMessages";
 
-describe(`calcXY tests`, () => {
-    it(`Valid result`, () => {
-        expect(()=> calcXY(10, 589, [10, 10], 60, 6, 0, 0, 0, 0 ).toBe( !0));
-    });
+describe(`calcXY`, () => {
+  it(`Should throw error when invalid rowHeight is passed`, () => {
+    expect(() => calcXY(10, 589, [10, 10], 0, 6, 10, 10, 1, 1))
+      .toThrowError(ErrorMsg.INVALID_PARAM_ROW_HEIGHT);
+  });
+
+  it(`Should throw error when invalid margin[0] is passed`, () => {
+    expect(() => calcXY(10, 589, [0, 10], 60, 6, 10, 10, 1, 1))
+      .toThrowError(ErrorMsg.INVALID_PARAM_MARGIN);
+  });
+
+  it(`Should throw error when invalid margin[1] is passed`, () => {
+    expect(() => calcXY(10, 589, [10, 0], 60, 6, 10, 10, 1, 1))
+      .toThrowError(ErrorMsg.INVALID_PARAM_MARGIN);
+  });
+
+  it(`Should throw error when invalid margin is passed`, () => {
+    expect(() => calcXY(10, 589, [0, 0], 60, 6, 10, 10, 1, 1))
+      .toThrowError(ErrorMsg.INVALID_PARAM_MARGIN);
+  });
+
+  it(`Should throw error when invalid innerH is passed`, () => {
+    expect(() => calcXY(10, 589, [10, 10], 10, 10, 0, 10, 1, 1))
+      .toThrowError(ErrorMsg.INVALID_PARAM_INNER_H);
+  });
+
+  it(`Should throw error when invalid innerW is passed`, () => {
+    expect(() => calcXY(10, 589, [10, 10], 10, 10, 10, 0, 1, 1))
+      .toThrowError(ErrorMsg.INVALID_PARAM_INNER_W);
+  });
+
+  it(`Should throw error when invalid cols is passed`, () => {
+    expect(() => calcXY(10, 589, [10, 10], 10, 0, 10, 10, 1, 1))
+      .toThrowError(ErrorMsg.INVALID_PARAM_COLS);
+  });
+
+  it(`Should throw error when invalid maxRows is passed`, () => {
+    expect(() => calcXY(10, 589, [10, 10], 10, 10, 10, 10, 0, 1))
+      .toThrowError(ErrorMsg.INVALID_PARAM_MAX_ROWS);
+  });
+
+  it(`Should throw error when invalid containerWidth is passed`, () => {
+    expect(() => calcXY(10, 589, [10, 10], 10, 10, 10, 10, 1, 0))
+      .toThrowError(ErrorMsg.INVALID_PARAM_CONTAINER_WIDTH);
+  });
 });
