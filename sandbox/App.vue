4be8d6f0b00937e858dd3bb0fdc7a6373456288d
gwinnem <gwinnem@gmail.com>
2024-02-06 16:43:21 +0100

    Updated test data

--- sandbox/App.vue
+++ sandbox/App.vue
@@ -226,7 +226,7 @@
 
 <script lang="ts" setup>
 import {computed, nextTick, onBeforeUnmount, onMounted, ref, Ref, UnwrapRef} from 'vue';
-import {testDataOne as testData} from './test';
+import {testDataTwo as testData} from './test';
 import GridLayout from '../src/components/Grid/GridLayout.vue';
 import GridItem from '../src/components/Grid/GridItem.vue';
 import {ILayoutItem, TLayout} from "@/components/Grid/layout-definition";
